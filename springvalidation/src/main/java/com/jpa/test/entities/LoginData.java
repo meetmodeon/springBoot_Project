d55710b9148216bf9172fbd5be2947bf44039cc4
Meet_Yadav <120273933+meetmodeon@users.noreply.github.com>
2024-06-16 17:09:49 +0545

    form validation using spring mvc validation

---
+++ springvalidation/src/main/java/com/jpa/test/entities/LoginData.java
@@ -0,0 +1,51 @@
+package com.jpa.test.entities;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class LoginData {
+	
+	
+	@NotBlank(message="User Name can not be empty!!")
+	@Size(min=3,max=12,message="Username name must be between 3 -12 characters !!")
+	private String userName;
+	
+	@Pattern(regexp= "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$",message="Invalid Email")
+	private String email;
+	
+	@AssertTrue(message= "Must agree terms and condition")
+	private boolean agreed;
+	
+	
+	public boolean isAgreed() {
+		return agreed;
+	}
+	public void setAgreed(boolean agreed) {
+		this.agreed = agreed;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "LoginData [userName=" + userName + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+
+}
